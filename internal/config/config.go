ed947c8afebf8086ad4655522de9bad41f2105f8
minxrka <minorka04@mail.ru>
2025-05-23 21:14:41 +0300

    *config

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,6 +1,12 @@
 package config
 
-import "time"
+import (
+	"flag"
+	"os"
+	"time"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
 
 type Config struct {
 	Env      string        `yaml:"env" env-default:"local"`
@@ -12,4 +18,35 @@ type Config struct {
 type GRCPConfig struct {
 	GRPCPort int           `yaml:"grpc_port"`
 	Timeout  time.Duration `yaml:"timeout"`
+}
+
+func MustLoad() *Config {
+	path := fetchConfigPath()
+
+	if path == "" {
+		panic("config path is empty")
+	}
+
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		panic("config file not found" + path)
+	}
+
+	var cfg Config
+	if err := cleanenv.ReadConfig(path, &cfg); err != nil {
+		panic(err)
+	}
+	return &cfg
+}
+
+func fetchConfigPath() string {
+	var res string
+
+	flag.StringVar(&res, "config", "", "path to config file")
+	flag.Parse()
+
+	if res == "" {
+		return os.Getenv("CONFIG_PATH")
+	}
+
+	return res
 }
