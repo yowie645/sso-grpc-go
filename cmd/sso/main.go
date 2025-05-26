68faaba39b125c76f34276cd112e212251d3f40a
minxrka <minorka04@mail.ru>
2025-05-26 18:04:10 +0300

    *main *config

--- cmd/sso/main.go
+++ cmd/sso/main.go
@@ -1,11 +1,48 @@
-package sso
+package main
+
+import (
+	"fmt"
+	"log/slog"
+	"os"
+
+	"github.com/yowie645/sso-grpc-go/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
 
 func main() {
-	// TODO инициализация объект конфига
+	cfg := config.MustLoad()
+	fmt.Println(cfg)
+	log := setupLogger(cfg.Env)
+	log.Info("start app", slog.String("env", cfg.Env), slog.Any("cfg", cfg), slog.Int("port", cfg.GRPC.Port))
+
+	log.Debug("debug message")
+
+	log.Error("error message")
+
+	log.Warn("warn message")
 
 	// TODO инициализация логгера
 
 	// TODO инициализация приложения
 
 	// TODO запуск приложения
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envDev:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envProd:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+	return log
 }
--- internal/config/config.go
+++ internal/config/config.go
@@ -10,43 +10,41 @@ import (
 
 type Config struct {
 	Env      string        `yaml:"env" env-default:"local"`
-	Storage  string        `yaml:"storage" end-required:"true"`
+	Storage  string        `yaml:"storage" env-required:"true"`
 	TokenTTL time.Duration `yaml:"token_ttl" env-default:"1h"`
-	GRCP     GRCPConfig
+	GRPC     GRPCConfig    `yaml:"grpc"`
 }
 
-type GRCPConfig struct {
-	GRPCPort int           `yaml:"grpc_port"`
-	Timeout  time.Duration `yaml:"timeout"`
+type GRPCConfig struct {
+	Port    int           `yaml:"port"`
+	Timeout time.Duration `yaml:"timeout"`
 }
 
 func MustLoad() *Config {
 	path := fetchConfigPath()
-
 	if path == "" {
 		panic("config path is empty")
 	}
 
 	if _, err := os.Stat(path); os.IsNotExist(err) {
-		panic("config file not found" + path)
+		panic("config file not found: " + path)
 	}
 
 	var cfg Config
 	if err := cleanenv.ReadConfig(path, &cfg); err != nil {
-		panic(err)
+		panic("failed to read config: " + err.Error())
 	}
+
 	return &cfg
 }
 
 func fetchConfigPath() string {
 	var res string
-
 	flag.StringVar(&res, "config", "", "path to config file")
 	flag.Parse()
 
 	if res == "" {
 		return os.Getenv("CONFIG_PATH")
 	}
-
 	return res
 }
