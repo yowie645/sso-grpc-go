d90c3bcdd15b81e82b9825022dbaaeedede5a19d
minxrka <minorka04@mail.ru>
2025-05-31 17:59:18 +0300

    *auth(register)

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/yowie645/sso-grpc-go
 
-go 1.23
+go 1.23.0
 
 toolchain go1.23.9
 
@@ -18,10 +18,10 @@ require (
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/yowie645/protos-sso-grcp-go v0.0.0-20250521134019-711f20da6ab1 // indirect
-	golang.org/x/crypto v0.33.0 // indirect
+	golang.org/x/crypto v0.38.0 // indirect
 	golang.org/x/net v0.35.0 // indirect
-	golang.org/x/sys v0.30.0 // indirect
-	golang.org/x/text v0.22.0 // indirect
+	golang.org/x/sys v0.33.0 // indirect
+	golang.org/x/text v0.25.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20250218202821-56aae31c358a // indirect
 	google.golang.org/grpc v1.72.2 // indirect
 	google.golang.org/protobuf v1.36.6 // indirect
--- internal/services/auth/auth.go
+++ internal/services/auth/auth.go
@@ -2,15 +2,57 @@ package auth
 
 import (
 	"context"
+	"fmt"
 	"log/slog"
 	"time"
+
+	"github.com/yowie645/sso-grpc-go/internal/domain/models"
+	"golang.org/x/crypto/bcrypt"
 )
 
 // Auth - (пока заглушка)
 type Auth struct {
 	log         *slog.Logger
+	usrSaver    UserSaver
+	usrProvider UserProvider
+	appProvider AppProvider
 	storagePath string
 	tokenTTL    time.Duration
+}
+
+type UserSaver interface {
+	SaveUser(
+		ctx context.Context,
+		email string,
+		passHash []byte,
+	) (uid int64, err error)
+}
+
+type UserProvider interface {
+	GetUser(ctx context.Context, email string) (userID int64, err error)
+}
+
+type AppProvider interface {
+	App(ctx context.Context, appID int) (models.App, error)
+}
+
+// New returns a new instance of the Auth service
+func New(
+	log *slog.Logger,
+	userSaver UserSaver,
+	userProvider UserProvider,
+	appProvider AppProvider,
+	storagePath string,
+	tokenTTL time.Duration,
+) *Auth {
+	return &Auth{
+		usrSaver:    userSaver,
+		usrProvider: userProvider,
+		log:         log,
+		appProvider: appProvider,
+		storagePath: storagePath,
+		tokenTTL:    tokenTTL,
+	}
 }
 
 // IsAdmin implements auth.Auth.
@@ -19,20 +61,35 @@ func (a *Auth) IsAdmin(ctx context.Context, userID int64) (bool, error) {
 }
 
 // Login implements auth.Auth.
-func (a *Auth) Login(ctx context.Context, email string, password string, appID int) (token string, err error) {
+func (a *Auth) Login(ctx context.Context, email string, password string, appID int) (string, error) {
 	panic("unimplemented")
 }
 
 // RegisterNewUser implements auth.Auth.
-func (a *Auth) RegisterNewUser(ctx context.Context, email string, password string) (userID int64, err error) {
-	panic("unimplemented")
-}
+func (a *Auth) RegisterNewUser(ctx context.Context, email string, password string) (int64, error) {
+	const op = "auth.RegisterNewUser"
 
-// New - (пока заглушка)
-func New(log *slog.Logger, storagePath string, tokenTTL time.Duration) *Auth {
-	return &Auth{
-		log:         log,
-		storagePath: storagePath,
-		tokenTTL:    tokenTTL,
+	log := a.log.With(
+		slog.String("op", op),
+		slog.String("email", email),
+	)
+
+	log.Info("register new user")
+
+	passHash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+
+	if err != nil {
+		log.Error("failed to hash password", slog.String("error", err.Error()))
+		return 0, fmt.Errorf("%s: %w", op, err)
 	}
+
+	id, err := a.usrSaver.SaveUser(ctx, email, passHash)
+	if err != nil {
+		log.Error("failed to save user", slog.String("error", err.Error()))
+		return 0, fmt.Errorf("%s: %w", op, err)
+	}
+
+	log.Info("registered new user")
+
+	return id, nil
 }
