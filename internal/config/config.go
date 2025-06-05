2eb905b1fdc088e40e60dabcff1943f919987ada
minxrka <minorka04@mail.ru>
2025-06-05 17:04:13 +0300

    *config +tests +suite

--- internal/config/config.go
+++ internal/config/config.go
@@ -22,18 +22,24 @@ type GRPCConfig struct {
 }
 
 func MustLoad() *Config {
-	path := fetchConfigPath()
-	if path == "" {
+	configPath := fetchConfigPath()
+	if configPath == "" {
 		panic("config path is empty")
 	}
 
-	if _, err := os.Stat(path); os.IsNotExist(err) {
-		panic("config file not found: " + path)
+	return MustLoadByPath(configPath)
+}
+
+func MustLoadByPath(configPath string) *Config {
+	// check if file exists
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		panic("config file does not exist: " + configPath)
 	}
 
 	var cfg Config
-	if err := cleanenv.ReadConfig(path, &cfg); err != nil {
-		panic("failed to read config: " + err.Error())
+
+	if err := cleanenv.ReadConfig(configPath, &cfg); err != nil {
+		panic("cannot read config: " + err.Error())
 	}
 
 	return &cfg
---
+++ tests/suite/suite.go
@@ -0,0 +1,54 @@
+package suite
+
+import (
+	"context"
+	"net"
+	"strconv"
+	"testing"
+
+	"github.com/yowie645/sso-grpc-go/internal/config"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
+	authv1 "github.com/yowie645/protos-sso-grcp-go/gen/go/sso"
+)
+
+type Suite struct {
+	*testing.T
+	Cfg        *config.Config
+	AuthClient authv1.AuthClient
+}
+
+const (
+	grpcHost = "localhost"
+)
+
+func New(t *testing.T) (context.Context, *Suite) {
+	t.Helper()
+	t.Parallel()
+
+	cfg := config.MustLoadByPath("../config/local_tests.yaml")
+
+	ctx, cancelCtx := context.WithTimeout(context.Background(), cfg.GRPC.Timeout)
+
+	t.Cleanup(func() {
+		t.Helper()
+		cancelCtx()
+	})
+
+	cc, err := grpc.DialContext(context.Background(),
+		grpcAddress(cfg), grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		t.Fatalf("grpc server connections failed: %v", err)
+	}
+
+	return ctx, &Suite{
+		T:          t,
+		Cfg:        cfg,
+		AuthClient: authv1.NewAuthClient(cc),
+	}
+}
+
+func grpcAddress(cfg *config.Config) string {
+	return net.JoinHostPort(grpcHost, strconv.Itoa(cfg.GRPC.Port))
+}
